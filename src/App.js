fdf982da718536b731cb0414e84a948ac6281316
nadim184 <51932411+nadim184@users.noreply.github.com>
2019-07-16 15:56:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import photo from './logo.svg';
 import './App.css';
-import algorythme from './azerty.png'
-import Component from './component';
-import Container from './container'; 
-
+import Wrapper from './Wrapper'
 function App() {
   return (
    <div className="App">
     
       
-      <header className="App-header">
-         <div className="init">
-         <Container/>
-    <Container/>
-    <Container/>
-    <Container/>
-     </div>
-<div className="st" >
-	<h1 className="title red"> WELCOME </h1>
-
-	
+  
+<Wrapper />
   
 
-<img src={algorythme} />
-
-
-
-	<img src={photo} />
-  </div>
-
-</header>
 
 
 </div>
--- src/component.js
+++
@@ -1,6 +0,0 @@
-  import React from 'react' ;
-  function Component(){
-      let name = "hello";
-      return (<h1>{name} world</h1>)
-  }
-  export default Component;
